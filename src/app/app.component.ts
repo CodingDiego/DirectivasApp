d3f9565dfb08552a07a2962f3b2ff57de84451d6
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-11 05:21:15 -0300

    ADD: ngIf Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DirectivasApp';
+  title = 'Registro de usuarios';
+  mensaje = ""
+  registrado = false;
+
+  registrarUsuario(){
+    this.registrado = true;
+    this.mensaje = "Usuario registrado con exito"
+  }
+
 }
