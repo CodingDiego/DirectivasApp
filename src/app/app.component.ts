0e105cda3f8e7442d462cac4e7304b6de9232801
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-11 07:06:09 -0300

    ADD: Style and ngFor Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,10 +13,28 @@ export class AppComponent {
   title = 'Registro de usuarios';
   mensaje = ""
   registrado = false;
+  nombre:string = "";
+  apellido:string = "";
+
+  entradas: object[];
+
+  constructor(){
+    this.entradas = [
+      {titulo:"Python cada dia mas inutil"},
+      {titulo:"C++ cada dia mas usado por los indios"},
+      {titulo:"JavaScript cada dia mas vanilla"},
+      {titulo:"Java cada dia consumiendo mas RAM"},
+      {titulo:"El gobierno de Massa cada dia mas nefasto"},
+      {titulo:"Los peronistas cada dia mas mogolicos"},
+      {titulo:"Viva la Libertad carajo"},
+    ]
+    
+  }
 
   registrarUsuario(){
+
     this.registrado = true;
-    this.mensaje = "Usuario registrado con exito"
+    this.mensaje = `${this.nombre} ${this.apellido} ha sido registrado con exito`;
   }
 
 }
