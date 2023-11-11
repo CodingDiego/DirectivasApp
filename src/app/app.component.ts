b0b744f9013b11f6bb490cae845b8a53d5668935
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-11 07:16:28 -0300

    No message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Registro de usuarios';
   mensaje = ""
